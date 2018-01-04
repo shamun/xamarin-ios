fc5b19adc3778bbced76bc9b93d0e4026e6fd96f
sun <sun@suns-MacBook-Air.local>
2018-01-04 14:19:51 +0100

    ios

---
+++ ios-callkit/ios-callkit/Main.cs
@@ -0,0 +1,10 @@
+﻿using UIKit;
+
+namespace ioscallkit {
+  public class Application {   
+    static void Main(string[] args) {
+      
+      UIApplication.Main(args, null, "AppDelegate");
+    }
+  }
+}
---
+++ ios-callkit/ios-callkit/ViewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using UIKit;
+
+namespace ioscallkit {
+  public partial class ViewController : UIViewController {
+    protected ViewController(IntPtr handle) : base(handle) {
+      
+    }
+
+    public override void ViewDidLoad() {
+      base.ViewDidLoad();
+
+    }
+
+    public override void DidReceiveMemoryWarning() {
+      base.DidReceiveMemoryWarning();
+
+    }
+  }
+}
